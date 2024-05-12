751da1542c032ddb3fdd4dfbde4fd60dbf64c578
ChrisM345 <chrism.12277@gmail.com>
2024-05-12 18:06:26 -0500

    Added test data for resume fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Applicant from "./components/applicant";
 import Application from "./components/application";
 import { Education, Skills, Experience } from "./components/application";
+import EducationForm from "./components/education";
 import { useState } from "react";
 import "./styles/App.css";
 
@@ -13,15 +14,34 @@ function App() {
     summary: "Example summary",
   });
 
-  const [educationData, setEducationData] = useState("");
-  const [skillsData, setSkillsData] = useState("");
-  const [experienceData, setExperienceData] = useState("");
+  const [educationData, setEducationData] = useState({
+    school: "School Name",
+    city: "City",
+    state: "State",
+    graduation: "Graduation Month and Year",
+    degree: "Degree",
+    gpa: "GPA",
+  });
+
+  const [skillsData, setSkillsData] = useState({
+    skills: ["test1", "test2", "test3"],
+  });
+
+  const [experienceData, setExperienceData] = useState({
+    jobTitle: ["jobtitle1", "jobtitle2"],
+    companyName: ["company1", "company2"],
+    jobResponsibilities: [
+      ["Managed a team", "Trained others"],
+      ["Worked", "Led a team"],
+    ],
+  });
 
   return (
     <div className="container">
       <div className="form-fields">
         <h1>CV Application</h1>
         <Applicant data={setApplicantData} />
+        <EducationForm data={setEducationData} />
       </div>
       <div className="cv-application">
         <Application data={applicantData} />
