f59b95a9c65e3785b27cfbf86eaf593ab0c4b3d0
ChrisM345 <chrism.12277@gmail.com>
2024-05-18 13:20:51 -0500

    Added options to delete jobs and responsibilities. Added option to add more jobs

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -13,12 +13,39 @@ function ExperienceForm({ data }) {
     jobResponsibilities: jobResponsibilities,
   });
 
+  const [newJobTitle, setNewJobTitle] = useState("");
+  const [newCompanyName, setNewCompanyName] = useState("");
+  const [newResponsibility, setNewResponsibility] = useState("");
   const [showSubmit, setShowSubmit] = useState(true);
+
+  function handleNewJobTitle(e) {
+    setNewJobTitle(e.target.value);
+  }
 
   function handleJobTitleChange(e, index) {
     let copiedJobs = [...jobTitles];
     copiedJobs[index] = e.target.value;
     setJobTitles(copiedJobs);
+  }
+
+  function handleAddJob() {
+    setJobTitles([...jobTitles, newJobTitle]);
+    setCompanyNames([...companyNames, newCompanyName]);
+    setJobResponsibilities([...jobResponsibilities, [newResponsibility]]);
+    document.getElementById("newJobTitle").value = "";
+    document.getElementById("newCompanyName").value = "";
+    document.getElementById("newJobResponsibility").value = "";
+  }
+
+  function handleJobDelete(index) {
+    console.log("deleting job");
+    setJobTitles(jobTitles.slice(0, index).concat(jobTitles.slice(index + 1)));
+    setCompanyNames(companyNames.slice(0, index).concat(companyNames.slice(index + 1)));
+    setJobResponsibilities(jobResponsibilities.slice(0, index).concat(jobResponsibilities.slice(index + 1)));
+  }
+
+  function handleNewCompanyName(e) {
+    setNewCompanyName(e.target.value);
   }
 
   function handleCompanyNameChange(e, index) {
@@ -31,6 +58,26 @@ function ExperienceForm({ data }) {
     let copiedJobResponsibilities = [...jobResponsibilities];
     console.log(copiedJobResponsibilities[jobIndex][index]);
     copiedJobResponsibilities[jobIndex][index] = e.target.value;
+    setJobResponsibilities(copiedJobResponsibilities);
+  }
+
+  function handleNewResponsibility(e) {
+    setNewResponsibility(e.target.value);
+  }
+
+  function handleAddResponsibility(e) {
+    const index = e.target.parentNode.id;
+    let copiedJobResponsibilities = [...jobResponsibilities];
+    copiedJobResponsibilities[index] = copiedJobResponsibilities[index].concat(newResponsibility);
+    setJobResponsibilities(copiedJobResponsibilities);
+    document.getElementById("newResponsibility").value = "";
+  }
+
+  function handleDeleteResponsibility(outerIndex, innerIndex) {
+    let copiedJobResponsibilities = [...jobResponsibilities];
+    copiedJobResponsibilities[outerIndex] = copiedJobResponsibilities[outerIndex]
+      .slice(0, innerIndex)
+      .concat(copiedJobResponsibilities[outerIndex].slice(innerIndex + 1));
     setJobResponsibilities(copiedJobResponsibilities);
   }
 
@@ -50,21 +97,36 @@ function ExperienceForm({ data }) {
   return showSubmit ? (
     <>
       <h2>Experience Form</h2>
+      <h3>Current Experience</h3>
       <form onSubmit={(e) => e.preventDefault()}>
         {jobTitles.map((job, index) => {
           return (
-            <div className="jobData" key={index}>
+            <div className="jobData" id={index} key={index}>
+              <button onClick={() => handleJobDelete(index)}>Delete Job</button>
               <input placeholder="Job Title" value={job} onChange={(e) => handleJobTitleChange(e, index)} />
               <input placeholder="Company Name" value={companyNames[index]} onChange={(e) => handleCompanyNameChange(e, index)} />
               {jobResponsibilities[index].map((responsibility, idx) => {
                 return (
-                  <input placeholder="Job Responsibility" value={responsibility} onChange={(e) => handleJobResponsibilityChange(e, index, idx)} />
+                  <>
+                    <input placeholder="Job Responsibility" value={responsibility} onChange={(e) => handleJobResponsibilityChange(e, index, idx)} />
+                    <button onClick={() => handleDeleteResponsibility(index, idx)}>Delete</button>
+                  </>
                 );
               })}
+              <input placeholder="Responsibility" id="newResponsibility" onChange={handleNewResponsibility} />
+              <button onClick={(e) => handleAddResponsibility(e)}>Add Responsibility</button>
             </div>
           );
         })}
-        <button onClick={handleSendData}>Submit</button>
+        <h3>Add Experience</h3>
+        <input placeholder="Job Title" id="newJobTitle" onChange={handleNewJobTitle} />
+        <input placeholder="Company Name" id="newCompanyName" onChange={handleNewCompanyName} />
+        <input placeholder="Job Responsibility" id="newJobResponsibility" onChange={handleNewResponsibility} />
+        <button onClick={handleAddJob}>Add Job</button>
+        <div>
+          <h3>Submit Experience Form</h3>
+          <button onClick={handleSendData}>Submit</button>
+        </div>
       </form>
     </>
   ) : (
