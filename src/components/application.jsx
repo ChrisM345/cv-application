3b8e93748f98c2851dd4e53bba990fc2728c8c17
ChrisM345 <chrism.12277@gmail.com>
2024-05-20 17:49:15 -0500

    Cleanup and grabbing month and year data for job start and end date

--- src/components/application.jsx
+++ src/components/application.jsx
@@ -1,59 +1,79 @@
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+// Basic Info for the CV Application
 function Application({ data: { name, phoneNumber, email, linkedin, summary } }) {
   return (
     <>
-      <h1>CV Application</h1>
-      <h2>{name}</h2>
-      <div>
-        {phoneNumber}, {email}, {linkedin}
+      <div className="basic-info">
+        <h2>{name}</h2>
+        <div>
+          {phoneNumber}, {email}, {linkedin}
+        </div>
       </div>
-      <h3>Summary</h3>
+      <h3 className="h3-cv-application">Summary</h3>
       {summary}
     </>
   );
 }
 
+// Education Info for the CV Application
 function Education({ data: { school, city, state, graduation, degree, gpa } }) {
   return (
     <>
-      <h3>Education</h3>
-      <div>
-        {school}, {city}, {state} <span>{graduation}</span>
-        <br></br>
-        {degree} | {gpa}
+      <div className="education-info">
+        <h3 className="h3-cv-application">Education</h3>
+        <div>
+          <b>
+            {school}, {city}, {state} | Graduated <span>{graduation}</span>
+          </b>
+          <br></br>
+          {degree} | GPA {gpa}
+        </div>
       </div>
     </>
   );
 }
 
+// Skills Info for the CV Application
 function Skills({ data: { skills } }) {
   return (
     <>
-      <h3>Skills</h3>
-      {skills.join(", ")}
+      <div className="skills-info">
+        <h3 className="h3-cv-application">Skills</h3>
+        {skills.join(", ")}
+      </div>
     </>
   );
 }
 
-function Experience({ data: { jobTitle, companyName, jobResponsibilities } }) {
+// Experience Info for the CV Application
+function Experience({ data: { jobTitle, companyName, companyLocation, jobStartDate, jobEndDate, jobResponsibilities } }) {
   return (
     <>
-      <h3>Experience</h3>
-      {jobTitle.map((job, idx) => {
-        {
-          return (
-            <>
-              <h4>
-                {job}, {companyName[idx]}
-              </h4>
-              <ul>
-                {jobResponsibilities[0].map((responsibility) => {
-                  return <li key={responsibility}>{responsibility}</li>;
-                })}
-              </ul>
-            </>
-          );
-        }
-      })}
+      <div className="experience-info">
+        <h3 className="h3-cv-application">Experience</h3>
+        {jobTitle.map((job, idx) => {
+          {
+            return (
+              <>
+                <h4 className="h4-cv-application">
+                  {job}, {companyName[idx]} | {companyLocation[idx]} | {months[parseInt(jobStartDate[idx].split("-")[1])]}{" "}
+                  {jobStartDate[idx].split("-")[0]}-{months[parseInt(jobEndDate[idx].split("-")[1])]} {jobEndDate[idx].split("-")[0]}
+                </h4>
+                <ul className="ul-li-jobs">
+                  {jobResponsibilities[idx].map((responsibility) => {
+                    return (
+                      <li className="ul-li-jobs" key={responsibility}>
+                        {responsibility}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </>
+            );
+          }
+        })}
+      </div>
     </>
   );
 }
