74d58a77e80cb0dfd1f690bdd6746628e4d57da9
ChrisM345 <chrism.12277@gmail.com>
2024-05-20 17:50:07 -0500

    Cleanup and checks to ensure all fields are filled out

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,17 +1,10 @@
 import { useState } from "react";
+import "../styles/forms.css";
 
 function SkillForm({ data }) {
   const [skills, setSkills] = useState([]);
   const [skillName, setSkillName] = useState("");
   const [showSubmit, setShowSubmit] = useState(true);
-
-  function handleSkillNameChange(e, index) {
-    const clonedData = [...skills];
-    console.log(clonedData[index]);
-
-    // clonedData[index][e.target.name] = e.target.value;
-    // setSkillName(e.target.value);
-  }
 
   function handleNewSkillChange(e) {
     setSkillName(e.target.value);
@@ -19,57 +12,69 @@ function SkillForm({ data }) {
 
   function handleAddSkill() {
     setSkills([...skills, skillName]);
+    // Set the value to "" for the new input that appear
     document.getElementById("newSkill").value = "";
   }
 
+  // skills is an array so we need to keep track of the index
   function handleSkillsChange(e, index) {
-    console.log(e);
-    console.log(index);
     let copiedSkills = [...skills];
     copiedSkills[index] = e.target.value;
     setSkills(copiedSkills);
   }
 
-  function handleSetSubmit() {
-    setShowSubmit(!showSubmit);
+  // Commit changes and update CV Application
+  function handleSendData() {
+    if (skills.length != 0) {
+      data({
+        skills: skills,
+      });
+      handleSetSubmit();
+    }
   }
 
-  function handleSendData() {
-    data({
-      skills: skills,
-    });
-    handleSetSubmit();
+  // Switches between Submit and Edit button
+  function handleSetSubmit() {
+    setShowSubmit(!showSubmit);
   }
 
   return showSubmit ? (
     <>
       <h2>Skills Form</h2>
-      {skills}
+      Current Skills: {skills.join(", ")}
       <form onSubmit={(e) => e.preventDefault()}>
         {skills.map((skill, index) => {
           return (
             <>
-              <input key={index} placeholder="Skill" value={skill} onChange={(e) => handleSkillsChange(e, index)} />
-              <button
-                onClick={() => {
-                  setSkills(skills.slice(0, index).concat(skills.slice(index + 1)));
-                }}
-              >
-                Delete
-              </button>
+              <div className="skills-form">
+                <input key={index} placeholder="Skill" value={skill} onChange={(e) => handleSkillsChange(e, index)} />
+                <button
+                  onClick={() => {
+                    setSkills(skills.slice(0, index).concat(skills.slice(index + 1)));
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
             </>
           );
         })}
         <input id="newSkill" placeholder="Skill" onChange={handleNewSkillChange} />
         <button onClick={handleAddSkill}>Add Skill</button>
-        <button onClick={handleSendData}>Submit</button>
+        <div>
+          <p>
+            Submit Skills Form: <button onClick={handleSendData}>Submit</button>
+          </p>
+        </div>
       </form>
     </>
   ) : (
     <>
       <h2>Skills Form</h2>
       <form onSubmit={(e) => e.preventDefault()}>
-        <button onClick={handleSetSubmit}>Edit</button>
+        <p>
+          Edit Skills Form: <button onClick={handleSetSubmit}>Edit</button>
+        </p>
       </form>
     </>
   );
